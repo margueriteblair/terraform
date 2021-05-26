6f72e9b3ac44c31bc6a5b1b3fa80426e75353325
Marguerite Blair <marguerite.blair@gmail.com>
2021-05-26 16:34:58 -0400

    fixing shit

--- main.tf
+++ main.tf
@@ -29,10 +29,10 @@ resource "aws_vpc" "first-vpc" {
   }
 }
 
-resource "aws_subnet" "main" {
-  vpc_id = "${aws_vpc.main.id}"
+resource "aws_subnet" "subnet-1" {
+  vpc_id = aws_vpc.first-vpc.id
   cidr_block = "10.0.1.0/24"
   tags = {
-    Name = "Main"
+    Name = "prod-subnet
   }
 }
