78cf24d1845ca65b97940b3cc45fd93e3327e4da
Marguerite Blair <marguerite.blair@gmail.com>
2021-05-27 15:06:51 -0400

    eip now depends on the gateway

--- main.tf
+++ main.tf
@@ -118,4 +118,7 @@ resource "aws_eip" "one" {
   vpc = true
   network_interface = aws_network_interface.web-server-nic.id
   associate_with_private_ip = "10.0.1.50"
+  depends_on = aws_internet_gateway.gw
 }
+
+#last step now is to actually create the ubuntu server
