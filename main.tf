275d1d332ab7f4a050c66d99b6e450690a667573
Marguerite Blair <marguerite.blair@gmail.com>
2021-05-27 14:54:11 -0400

    set up egress and ingress web policy

--- main.tf
+++ main.tf
@@ -57,29 +57,48 @@ resource "aws_route_table_association" "a" {
 }
 
 #now create a security group
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.main.id
+#
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web_traffic"
+  description = "Allow Web traffic"
+  vpc_id      = aws_vpc.prod-vpc.id
 
+  #ingress policy and egress policy
   ingress {
-    description      = "TLS from VPC"
+    description      = "HTTPS"
     from_port        = 443
     to_port          = 443
     protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.main.cidr_block]
+    cidr_blocks      = [0.0.0.0/0]
+    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = [0.0.0.0/0]
+    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [0.0.0.0/0]
     ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
   }
 
+  #we want to connect to any
   egress {
     from_port        = 0
     to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    protocol         = "-1" #any protocol
+    cidr_blocks      = ["0.0.0.0/0"] #any IP address
     ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
-    Name = "allow_tls"
+    Name = "allow_web"
   }
 }
