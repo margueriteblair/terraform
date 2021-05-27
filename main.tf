62b3ae51ae70660408191b400a882a99635cffb5
Marguerite Blair <marguerite.blair@gmail.com>
2021-05-27 14:17:01 -0400

    security group config

--- main.tf
+++ main.tf
@@ -50,7 +50,36 @@ resouce "aws_subnet" "subnet-1" {
   }
 }
 
+#route tables association with the subnet
 resource "aws_route_table_association" "a" {
   subnet_id = aws_subnet.subnet-1.id
   router_table_id = aws_route_table.prod-rt.id
+}
+
+#now create a security group
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.main.cidr_block]
+    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
 }
