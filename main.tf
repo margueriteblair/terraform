05977c007d352039f0f4312c02983626031f1f24
Marguerite Blair <marguerite.blair@gmail.com>
2021-05-28 14:02:27 -0400

    added tags so we can name our webserver

--- main.tf
+++ main.tf
@@ -138,6 +138,11 @@ resource "aws_instance" "web-server-instance" {
             sudo apt install apache2 -y
             sudo systemctl start apache2
             sudo bash -c 'echo your very first web server > /var/www/html/index.html' 
+            EOF
+  
+  tags = {
+    Name = "web-server"
+  }
 }
 
 #last step now is to actually create the ubuntu server
