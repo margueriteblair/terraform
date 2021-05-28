4d0e74bfcdad2fd6248b923cc3a992ecf8970fe2
Marguerite Blair <marguerite.blair@gmail.com>
2021-05-28 14:06:05 -0400

    few minor changes

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ resource "aws_route_table" "prod-rt" {
   }
 }
 
-resouce "aws_subnet" "subnet-1" {
+resource "aws_subnet" "subnet-1" {
   vpc_id = aws_vpc.prod-vpc.id
   cidr_block = "10.0.1.0/24"
   availability_zone = "us-east-2"
@@ -69,24 +69,24 @@ resource "aws_security_group" "allow_web" {
     from_port        = 443
     to_port          = 443
     protocol         = "tcp"
-    cidr_blocks      = [0.0.0.0/0]
-    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+    cidr_blocks      = ["0.0.0.0/0"]
+    # ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
   }
   ingress {
     description      = "HTTP"
     from_port        = 80
     to_port          = 80
     protocol         = "tcp"
-    cidr_blocks      = [0.0.0.0/0]
-    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+    cidr_blocks      = ["0.0.0.0/0"]
+    # ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
   }
   ingress {
     description      = "SSH"
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = [0.0.0.0/0]
-    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+    cidr_blocks      = ["0.0.0.0/0"]
+    # ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
   }
 
   #we want to connect to any
@@ -118,7 +118,7 @@ resource "aws_eip" "one" {
   vpc = true
   network_interface = aws_network_interface.web-server-nic.id
   associate_with_private_ip = "10.0.1.50"
-  depends_on = aws_internet_gateway.gw
+  depends_on =[aws_internet_gateway.gw]
 }
 
 resource "aws_instance" "web-server-instance" {
